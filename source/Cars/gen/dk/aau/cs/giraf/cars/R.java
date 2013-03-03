27d4636c2e35c5e099b12afb618888920c18aed6
Daniel Boecker Soerensen <dbso10@student.aau.dk>
2013-03-03 22:29:54 +0100

    button alignment update

--- source/Cars/gen/dk/aau/cs/giraf/cars/R.java
+++ source/Cars/gen/dk/aau/cs/giraf/cars/R.java
@@ -25,9 +25,9 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
-        public static final int button1=0x7f080000;
-        public static final int button2=0x7f080001;
-        public static final int button3=0x7f080002;
+        public static final int game_button=0x7f080000;
+        public static final int help_button=0x7f080002;
+        public static final int settings_button=0x7f080001;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
