414734375e8bd3a4c3bedf95daa59bef8b105c6d
D49L <dbso10@student.aau.dk>
2013-03-11 11:03:36 +0100

    Fragments

---
+++ Cars/src/dk/aau/cs/giraf/cars/Settings1Fragment.java
@@ -0,0 +1,21 @@
+package dk.aau.cs.giraf.cars;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Settings1Fragment extends Fragment {
+
+	
+		    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		      // Inflate the layout for this fragment
+		      View v = inflater.inflate(R.layout.fragment_settings1, container, false);
+		      return v;
+		   }
+		
+	
+}
