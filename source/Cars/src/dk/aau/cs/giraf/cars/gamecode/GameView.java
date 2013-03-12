7ed41bd981122a92037bed17dc7e512cb39dd1b9
Tom Reinholdt Petersen <tpeter09@student.aau.dk>
2013-03-12 15:56:49 +0100

    Lots of code

--- source/Cars/src/dk/aau/cs/giraf/cars/gamecode/GameView.java
+++ source/Cars/src/dk/aau/cs/giraf/cars/gamecode/GameView.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import javax.microedition.khronos.opengles.GL10;
 
+import dk.aau.cs.giraf.cars.R;
+import dk.aau.cs.giraf.cars.gamecode.GameObjects.Car;
+
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Rect;
 import android.opengl.GLSurfaceView;
 
 // TODO Add touch screen registration
@@ -31,7 +35,10 @@ public class GameView extends GLSurfaceView implements Drawer {
 
 	@Override
 	public void onDrawFrame(GL10 gl, GameRenderer spriteBatcher) {
-		// TODO Auto-generated method stub
+		//spriteBatcher.draw(gl, R.drawable.ic_launcher, new Rect(0, 0, 100, 100), new Rect(150, 150, 250, 250));
 		
+		for (IDrawable object : drawableObjects) {
+			object.Draw(gl, spriteBatcher);
+		}
 	}
 }
